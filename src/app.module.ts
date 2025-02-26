db1e2debcac29b2367989336b606fa36efee41f8
Kostya <kostyakazmiruk@gmail.com>
2025-02-26 20:03:36 +0200

    forgot to delete product module from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RecordingController } from './recording/recording.controller';
-import { RecordingService } from './recording/recording.service';
 import { RecordingModule } from './recording/recording.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'node:path';
@@ -22,7 +19,6 @@ import { join } from 'node:path';
       synchronize: true,
       autoLoadEntities: true,
     }),
-    ProductModule,
     RecordingModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client', 'build'),
